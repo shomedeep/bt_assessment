8063146645dc7f6f3db762046e67b1b3d99bd3c7
Shomedeep <shomedeepmondal@gmail.com>
2024-02-13 12:44:15 +0530

    error handling and validation added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,31 +1,66 @@
 const { v4: uuidv4 } = require("uuid");
+const Validator = require("fastest-validator");
 
 // In-memory database
 let users = [];
 
 // Get all users
-const getAllUser = async (req, res) => {
-  res.json(users);
+const getAllUser = async (req, res, next) => {
+  try {
+    res.status(200).json({
+      message: "User list fetched successfully",
+      users: users,
+      count: users.length,
+    });
+  } catch (err) {
+    const error = new Error("Internal Server Error", err);
+    error.status = 500;
+    next(error);
+  }
 };
 
 // Get a specific user by ID
 const getUser = async (req, res) => {
-  const user = users.find((user) => user.id === req.params.userId);
+  try {
+    const user = users.find((user) => user.id === req.params.userId);
 
-  if (user) {
-    res.json(user);
-  } else {
-    res.status(404).json({ error: "User not found" });
+    if (user && Object.keys(user).length) {
+      res.status(200).json({
+        message: "User fetched successfully",
+        user: user,
+      });
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  } catch (err) {
+    const error = new Error("Internal Server Error", err);
+    error.status = 500;
+    next(error);
   }
 };
 
 // Create a new user
-const createUser = async (req, res) => {
-  const { username, age, hobbies } = req.body;
+const createUser = async (req, res, next) => {
+  try {
+    const { username, age, hobbies } = req.body;
 
-  if (!username || !age) {
-    res.status(400).json({ error: "Username and age are required" });
-  } else {
+    const scehma = {
+      username: { type: "string", optional: false, max: "100" },
+      age: { type: "number", optional: false },
+      hobbies: { type: "array", optional: false },
+    };
+
+    const objClass = new Validator();
+    const validationResponse = objClass.validate(
+      { username, age, hobbies },
+      scehma
+    );
+    if (validationResponse !== true) {
+      res
+        .status(400)
+        .json({ message: "Validation failed", error: validationResponse });
+    }
+
     const newUser = {
       id: uuidv4(),
       username,
@@ -34,35 +69,69 @@ const createUser = async (req, res) => {
     };
 
     users.push(newUser);
-    res.status(201).json(newUser);
+    res.status(201).json({ message: "New user careated", user: newUser });
+  } catch (err) {
+    const error = new Error("Internal Server Error", err);
+    error.status = 500;
+    next(error);
   }
 };
 
 // Update an existing user
-const updateUser = async (req, res) => {
-  const { username, age, hobbies } = req.body;
-  const user = users.find((user) => user.id === req.params.userId);
+const updateUser = async (req, res, next) => {
+  try {
+    const { username, age, hobbies } = req.body;
 
-  if (user) {
-    user.username = username || user.username;
-    user.age = age || user.age;
-    user.hobbies = hobbies || user.hobbies;
+    const scehma = {
+      username: { type: "string", optional: false, max: "100" },
+      age: { type: "number", optional: false },
+      hobbies: { type: "array", optional: false },
+    };
 
-    res.json(user);
-  } else {
-    res.status(404).json({ error: "User not found" });
+    const objClass = new Validator();
+    const validationResponse = objClass.validate(
+      { username, age, hobbies },
+      scehma
+    );
+    if (validationResponse !== true) {
+      res
+        .status(400)
+        .json({ message: "Validation failed", error: validationResponse });
+    }
+
+    const user = users.find((user) => user.id === req.params.userId);
+
+    if (user) {
+      user.username = username || user.username;
+      user.age = age || user.age;
+      user.hobbies = hobbies || user.hobbies;
+
+      res.json({ message: "Item updated successfully", updatedUser: user });
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  } catch (err) {
+    const error = new Error("Internal Server Error", err);
+    error.status = 500;
+    next(error);
   }
 };
 
 // Delete an existing user
-const deleteUser = async (req, res) => {
-  const userIndex = users.findIndex((user) => user.id === req.params.userId);
+const deleteUser = async (req, res, next) => {
+  try {
+    const userIndex = users.findIndex((user) => user.id === req.params.userId);
 
-  if (userIndex !== -1) {
-    users.splice(userIndex, 1);
-    res.sendStatus(204);
-  } else {
-    res.status(404).json({ error: "User not found" });
+    if (userIndex !== -1) {
+      users.splice(userIndex, 1);
+      res.status(200).json({ message: "User deleted successfully" });
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  } catch (err) {
+    const error = new Error("Internal Server Error", err);
+    error.status = 500;
+    next(error);
   }
 };
 
